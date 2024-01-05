9b333c40cce26ad54b30764dd02e88e97ce7862f
James Link <james.link@medgurus.de>
2024-01-05 16:03:30 +0100

    add flash messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,24 @@ class ArticlesController < ApplicationController
         @articles = Article.all
     end
 
+    def new
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.create(article_params)
+        if @article.save
+            flash[:notice] = "Article was created succesfully."
+            redirect_to(@article) #(:action => 'show', id: article.id)
+        else
+            render "new", status: :unprocessable_entity
+        end
+    end
+     
+    private
+     
+    def article_params
+        params.require(:article).permit(:id, :title, :description)
+    end
+
 end
