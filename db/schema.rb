27f572b0d954d4e61ff1f91e58a7470c1cf3f04e
James Link <james.link@medgurus.de>
2024-01-05 11:46:07 +0100

    add articles params

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_04_114619) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_04_115703) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
